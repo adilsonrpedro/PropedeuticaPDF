deee636f3b4669e566197b3129bf87bbb427cb77
adilsonrpedro <adilsonrpedro@gmail.com>
2026-08-12 22:01:57 -0300

    corrigindo dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { DropZone } from './components/DropZone';
+import DropZone from './components/DropZone';
 import { ConvertSection } from './components/ConvertSection';
 import { OcrTab } from './components/OcrTab';
 import { TranscriptionTab } from './components/TranscriptionTab';
