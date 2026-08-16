136990d5fa17b382de19f4cfc1855c482350f9e4
adilsonrpedro <adilsonrpedro@gmail.com>
2026-08-16 15:30:19 -0300

    chave supabase moderação

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Coloque os seus dados reais do Supabase direto aqui dentro das aspas:
-const supabaseUrl = "https://qpoyijyqdlrsfzawwgou.supabase.co/rest/v1/";
+// Cole as suas credenciais reais aqui dentro das aspas:
+const supabaseUrl = "https://qpoyijyqdlrsfzawwgou.supabase.co";
 const supabaseAnonKey = "sb_publishable_UA7GPr8XuyQKem7SpI06ow_7cc84N-F";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
